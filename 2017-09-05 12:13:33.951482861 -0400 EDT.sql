1e22bc0ff7babb4e4a60ddb1c92da9185ed56c0c
ricky-clevinger <ricky-clevinger@cgi.com>
2017-09-05 12:54:15 -0400

    New statements

--- 2017-09-05 12:13:33.951482861 -0400 EDT.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO library VALUES(1, 'Lebanon Library', 'The Address', 7);
---
+++ 2017-09-05 12:28:37.417259 -0400 EDT.sql
@@ -0,0 +1,4 @@
+CREATE TABLE member (
+	Member_Id int(11) NOT NULL AUTO_INCREMENT,
+	MEMBER_Name varchar(45) NOT NULL
+);
